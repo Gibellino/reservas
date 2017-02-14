fa2bf1750eaa29462c8fef933c2fb9972dec7617
Gibellino <gibellino@gmail.com>
2017-02-14 15:09:54 +0000

    Formulario

---
+++ src/controller/GestaoMesas.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.ArrayList;
+
+import model.Mesa;
+
+public class GestaoMesas {
+
+	ArrayList<Mesa> mesas;
+	
+	public GestaoMesas(){
+		
+		mesas = new ArrayList<>();
+		
+	}
+
+	public void addMesa(int lugar, boolean fumador){
+		
+		Mesa m = new Mesa(mesas.size(),lugar, fumador);
+		
+		mesas.add(m);
+		
+	}
+	
+}
---
+++ src/view/Fmesas.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Fmesas extends Scene{
+
+	public Fmesas(double width, double height) {
+		Group gn;
+		super(new Group, width, height);
+		
+		gn = this.
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
